67558b3b3fcaca39ff25056ffa6429dcd6d6872f
Leo <leonard.njura@gmail.com>
2019-08-28 19:51:06 +0300

    ch(cleanup): Cleanup and redeploy - cleans up some dummy data - refreshes build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62cc27bb87768bbc4570dc2330964900.js"
+  "/precache-manifest.b609338151166a14ff67a47a7d154c7f.js"
 );
 
 self.addEventListener('message', (event) => {
